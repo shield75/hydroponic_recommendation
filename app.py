6d5ce6e8ab4a14855dc893b328b89ce25f465b50
shield75 <77926360+shield75@users.noreply.github.com>
2023-06-09 21:06:53 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,97 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import sklearn
+import pickle
+import pandas as pd
+from markupsafe import Markup
+import requests
+
+
+from utils.solution import fertilizer_dic
+
+app = Flask(__name__, static_url_path='/static')
+model = pickle.load(open('RandomForest.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+
+@app.route('/crp_rcmnd', methods =['POST'])
+def crp_rcmnd():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+    return render_template('predict.html', prediction_text="Recommended Crop: {}".format(prediction))
+
+
+@app.route('/rcmnd')
+def rcmnd():
+    return render_template('rcmnd.html')
+
+@ app.route('/solution_predict', methods=['POST'])
+def solution_recommend():
+
+    crop_name = str(request.form['crop'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorus'])
+    K = int(request.form['potassium'])
+    ph = float(request.form['ph'])
+
+    features = [N, P, K, ph]
+
+    current_fetures = np.array(features)
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+    phr = df[df['Crop'] == crop_name]['pH'].iloc[0]
+
+    features1 = [nr, pr, kr, phr]
+
+    good_features = np.array(features1)
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    ph1 = phr- ph
+
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K", abs(ph1):"ph"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+
+    elif max_value == "K":
+        if p < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    else:
+        if ph1 < 0:
+            key = 'PHHigh'
+        else:
+            key = "PHlow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('rcmnd.html', text1= "Current Parameters: {}".format(current_fetures), text2 = "Recommended Parameters: {}".format(good_features), recommendation=response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils/solution.py
@@ -0,0 +1,88 @@
+fertilizer_dic = {
+        'NHigh': """The N value of hydroponic system is high!!!.
+        <br/> High nitrogen levels can lead to excessive vegetative growth and imbalances in the plant's nutrient uptake. This can negatively affect crop productivity and quality. Please consider the following suggestions:
+
+        <br/><br/> 1. <i> Dilute the nutrient solution: </i> – Prepare a fresh batch of hydroponic nutrient solution with lower nitrogen concentrations. Follow the recommended nutrient ratios for the specific crop you are growing.
+
+        <br/> 2. <i>Adjust the pH: </i> – High nitrogen levels can cause the pH of the solution to rise. Measure the pH of the nutrient solution and adjust it to the appropriate range for your crop. This ensures optimal nutrient availability and uptake.
+
+        <br/>3. <i> Increase other nutrient levels:</i> – Excessive nitrogen can create imbalances with other essential nutrients. Check the nutrient levels of phosphorus (P), potassium (K), and micronutrients. Adjust their concentrations accordingly to maintain proper ratios.
+
+        <br/>4. <i>Implement a nitrogen sink:</i> - Introduce nitrogen-absorbing plants, such as lettuce or mustard greens, alongside your main crops. These plants act as "nitrogen sinks," helping to reduce the excess nitrogen levels in the solution.""",
+
+        'Nlow': """The N value of your hydroponic system is low!!!.
+        <br/>Insufficient nitrogen levels can result in stunted growth, reduced yields, and chlorosis (yellowing) of leaves. Please consider the following suggestions:
+        <br/><br/> 1. <i>Increase nitrogen concentration:</i> – Prepare a fresh batch of nutrient solution with higher nitrogen concentrations. Refer to the recommended nutrient ratios for your crop, ensuring a balanced mix of macronutrients and micronutrients.
+
+        <br/>2. <i>Supplement with nitrogen-rich fertilizers: </i> – If adjusting the nutrient solution alone doesn't provide sufficient nitrogen, consider using nitrogen-rich fertilizers. Organic sources like fish emulsion or blood meal can be added to the hydroponic system, following manufacturer instructions.
+
+        <br/>3. <i>Monitor and adjust:</i> – Regularly monitor the nitrogen levels in the hydroponic system using appropriate testing kits or sensors. Adjust the nutrient solution as necessary to maintain optimal nitrogen concentrations.
+
+        <br/>4. <i> Consider nitrogen fixation:</i> – Certain plants have the ability to fix atmospheric nitrogen, converting it into usable forms. Introducing nitrogen-fixing plants like legumes (e.g., peas, beans) into the hydroponic system can help increase nitrogen availability for other crops.""",
+
+        'PHigh': """The P value of your hydroponic system is high!!!.
+        <br/> Excessive phosphorus can lead to nutrient imbalances and adverse effects on plant health. Please consider the following suggestions:
+
+        <br/><br/>1. <i>Flush the system:</i> – Drain and replace the hydroponic nutrient solution with a fresh batch without excessive phosphorus. This will help remove the excess phosphorus and restore nutrient balance.
+
+        <br/>2. <i>Adjust the nutrient solution composition:</i> – Prepare a new nutrient solution with balanced nutrient ratios, ensuring a lower concentration of phosphorus. Follow the recommended nutrient ratios for your specific crop.
+
+        <br/>3. <i>Monitor and adjust pH:</i> – High phosphorus levels can affect the pH of the solution. Measure the pH and adjust it to the appropriate range for your crop. Maintaining the correct pH ensures optimal nutrient uptake.
+
+         <br/>4. <i>Check other nutrient levels:</i> – Excess phosphorus can interfere with the uptake of other nutrients, particularly micronutrients like iron (Fe) and zinc (Zn). Monitor their levels and adjust the nutrient solution composition accordingly.""",
+
+        'Plow': """The P value of your hydroponic system is low!!!.
+        <br/>Insufficient phosphorus can hinder plant growth, root development, and overall crop productivity. Please consider the following suggestions:
+
+        <br/><br/>1. <i>Increase phosphorus concentration:</i> – Prepare a fresh nutrient solution with a higher phosphorus concentration. Refer to the recommended nutrient ratios for your crop and ensure a balanced mix of macronutrients and micronutrients.
+
+        <br/>2. <i> Supplement with phosphorus-rich fertilizers:</i> – If adjusting the nutrient solution alone doesn't provide sufficient phosphorus, consider using phosphorus-rich fertilizers. Examples include monoammonium phosphate (MAP) or monopotassium phosphate (MKP). Follow the manufacturer's instructions for application rates.
+
+        <br/>3. <i>Phosphorus Fertilizers</i> – applying a fertilizer with a high phosphorous content in the NPK ratio (example: 10-20-10, 20 being phosphorous percentage).
+
+        <br/>4. <i>Monitor and adjust:</i> – Regularly monitor the phosphorus levels in the hydroponic system using appropriate testing kits or sensors. Adjust the nutrient solution as necessary to maintain optimal phosphorus concentrations.
+
+        <br/>5. <i>Enhance root uptake:</i> –Stimulate root development and phosphorus uptake by using root-promoting additives or beneficial microbes that facilitate nutrient absorption. This can help improve phosphorus availability to the plants.""",
+
+        'KHigh': """The K value of your hydroponic system is high!!!</b>.
+        <br/> Excessive potassium can disrupt nutrient balance and adversely affect plant health and growth. Please consider the following suggestions:
+
+        <br/><br/>1. <i>Flush the system: </i> Drain the hydroponic nutrient solution and replace it with a fresh batch without excessive potassium. This will help remove the excess potassium and restore nutrient balance.
+
+        <br/>2. <i>Adjust the nutrient solution composition:</i> - Prepare a new nutrient solution with balanced nutrient ratios, ensuring a lower concentration of potassium. Follow the recommended nutrient ratios for your specific crop.
+
+        <br/>3. <i>Monitor and adjust pH: </i> - High potassium levels can affect the pH of the solution. Measure the pH and adjust it to the appropriate range for your crop. Maintaining the correct pH ensures optimal nutrient uptake.
+
+        <br/>4. <iCheck other nutrient levels: </i> - Excess potassium can interfere with the uptake of other nutrients, such as calcium (Ca) and magnesium (Mg). Monitor their levels and adjust the nutrient solution composition accordingly. """,
+
+        'Klow': """The K value of your hydroponic system is low!!!.
+        <br/>Insufficient potassium can lead to stunted growth, reduced yields, and weakened plant defenses against stress.  Please consider the following suggestions:
+
+        <br/><br/>1. <i>Increase potassium concentration:</i> – Prepare a fresh nutrient solution with a higher potassium concentration. Refer to the recommended nutrient ratios for your crop and ensure a balanced mix of macronutrients and micronutrients.
+
+        <br/>2. <i> Supplement with potassium-rich fertilizers:</i> – If adjusting the nutrient solution alone doesn't provide sufficient potassium, consider using potassium-rich fertilizers. Examples include potassium nitrate or potassium sulfate. Follow the manufacturer's instructions for application rates.
+
+        <br/>3. <i>Monitor and adjust:</i> – Regularly monitor the potassium levels in the hydroponic system using appropriate testing kits or sensors. Adjust the nutrient solution as necessary to maintain optimal potassium concentrations.
+
+        <br/>4. <i>Enhance root uptake:</i> – Stimulate root development and potassium uptake by using root-promoting additives or beneficial microbes that facilitate nutrient absorption. This can help improve potassium availability to the plants.""",
+
+        'PHHigh': """The ph value of your hydroponic system is high!!!</b>.
+        <br/> High pH, or alkaline conditions, can cause nutrient imbalances and deficiencies as certain essential nutrients become less available to plants. Please consider the following suggestions:
+
+        <br/><br/>1. <i>pH adjustment:  </i> - Measure the pH of the hydroponic solution using a pH meter or pH testing kit. If the pH is too high, lower it by adding a pH down solution or acidifier. Phosphoric acid or nitric acid are commonly used to lower pH levels in hydroponics. Follow the product instructions and make gradual adjustments to avoid sudden pH swings.
+
+        <br/>2. <i>Monitor nutrient availability:</i> - High pH can result in reduced availability of certain nutrients, such as iron (Fe), manganese (Mn), and zinc (Zn). Supplement the hydroponic solution with chelated forms of these nutrients, which are more readily absorbed by plants under high pH conditions.
+
+        <br/>3. <i>Rinse growing medium: </i> - If the high pH persists, the pH of the growing medium can also be affected. Rinse the growing medium with pH-adjusted water to bring the pH back to the desired range. This helps ensure proper nutrient absorption. """,
+
+        'PHlow': """The ph value of your hydroponic system is low!!!.
+        <br/>Low pH, or acidic conditions, can lead to nutrient imbalances, nutrient toxicities, and hinder nutrient uptake. Please consider the following suggestions:
+
+        <br/><br/>1. <i>pH adjustment:</i> – Measure the pH of the hydroponic solution and if it is too low, raise it by adding a pH up solution or alkaline agent. Common alkaline agents used in hydroponics include potassium hydroxide (KOH) or calcium hydroxide (lime). Follow the product instructions and adjust gradually.
+
+        <br/>2. <i> Monitor nutrient availability:</i> – Low pH can increase the availability of certain nutrients, such as iron (Fe) and manganese (Mn), which may lead to toxicity. Monitor the levels of these nutrients and adjust the nutrient solution accordingly to avoid overloading plants with toxic concentrations.
+
+        <br/>3. <i>Rinse growing medium:</i> – If the low pH persists, rinse the growing medium with pH-adjusted water to bring the pH back to the desired range. This helps ensure optimal nutrient absorption.
+
+        <br/>4. <i>Regular pH monitoring: </i> – Regularly monitor the pH of the hydroponic solution to maintain it within the appropriate range for your specific crop. Aim for a pH range typically recommended for hydroponic systems, which is around 5.5 to 6.5 for most plants."""
+ }
